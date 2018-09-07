748e8b21425bd6db282bc969e2044cc9d322e295
Peiyance <30545288+Peiyance@users.noreply.github.com>
2018-09-07 13:48:46 +0800

    Update database.h

--- src/server/database.h
+++ src/server/database.h
@@ -1,12 +1,12 @@
 /********************************************************************************
  * Files         : database.h
- * Description   : the head file of database.c.
+ * Description   : the head file 
  * Author        : zpy
  * Last Modified : 2018.9.4
  ********************************************************************************/
 
 /********************************************************************************
- * Description : Declaration of all global variables in database.c. (add all global variables appearing in database.c here)
+ * Description : Declaration of all global variables 
  * Author      : zpy
  * Date        : 2018.9.4
  ********************************************************************************/
@@ -17,7 +17,7 @@ char password[] = "123";//password (sunjinda's birthday)(not anymore)
 char database[] = "realRosetta"; //database name (this is NOT a default database)
 
 /********************************************************************************
-Description : Declarations of all functions in databases.c (add all function appearing in database.c here)
+Description : Declarations of all functions 
 Author      : zpy
 Date        : 2018.9.4
 ********************************************************************************/
@@ -25,11 +25,3 @@ int check_signup(const char* line);
 int check_login(const char* line);
 int if_user_exist(const char* line);
 void insert_friend_into_database(const char* name1,const char* name2);
-/********************************************************************************
- Description : sign up in database
- Prameter    : Function is not completed.
- Return      : int (1 == success, 0 == failed)
- Side effect :
- Author      : zpy
- Date        : 2018.9.2
- ********************************************************************************/
